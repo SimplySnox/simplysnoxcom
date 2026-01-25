0d395893686bb39ac7f7aca42978c2639d18882a
snox <103229161+SimplySnox@users.noreply.github.com>
2026-01-25 15:11:00 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 // API
-import Fortune from "./pages/api/Fortune.jsx";
-import Roast from "./pages/api/Roast.jsx";
+import Fortune from "./pages/fun/Fortune.jsx";
+import Roast from "./pages/fun/Roast.jsx";
 // Tip
 import Tip from "./pages/Tip.jsx";
 import TipSuccess from "./pages/TipSuccess.jsx";
@@ -14,8 +14,8 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
 
-      <Route path="/api/fortune" element={<Fortune />} />
-      <Route path="/api/roast" element={<Roast />} />
+      <Route path="/fun/fortune" element={<Fortune />} />
+      <Route path="/fun/roast" element={<Roast />} />
 
       <Route path="/tip" element={<Tip />} />
       <Route path="/tip/success" element={<TipSuccess />} />
